f7b0d60b29fdf03b832ef28da8f333ff36aa7d0d
AssassinCrizR <skd.bwn96@gmail.com>
2018-08-08 14:49:16 +0530

    added snackbar for add to widget

--- app/src/main/java/notes/rednitrogen/com/rednotes/Notes.java
+++ app/src/main/java/notes/rednitrogen/com/rednotes/Notes.java
@@ -8,10 +8,12 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AlertDialog;
@@ -206,6 +208,11 @@ public class Notes extends AppCompatActivity {
                     Paper.book().write("title",note.getTitle());
                     Paper.book().write("note",note.getNote());
                     updateWidget();
+                    Snackbar snackbar = Snackbar.make(coordinatorLayout, "Note added to Widget", Snackbar.LENGTH_SHORT);
+                    View sbView = snackbar.getView();
+                    TextView textView = sbView.findViewById(android.support.design.R.id.snackbar_text);
+                    textView.setTextColor(Color.YELLOW);
+                    snackbar.show();
                 }
                 else {
                     deleteNote(position);
