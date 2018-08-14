46a2e1fb1a17b9d90a8da2951cfa0fc27e702480
AssassinCrizR <skd.bwn96@gmail.com>
2018-08-14 18:28:31 +0530

    tasks in progress

---
+++ app/src/main/java/notes/rednitrogen/com/rednotes/Tasks.java
@@ -0,0 +1,16 @@
+package notes.rednitrogen.com.rednotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class Tasks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasks);
+        Toolbar toolbar = findViewById(R.id.toolbar_tasks);
+        setSupportActionBar(toolbar);
+    }
+}
